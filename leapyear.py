b488d7826340ac93f3703a4cc7b4c72c02fbf2a6
Chandana C Sagar <130214038+Chandanacsagar@users.noreply.github.com>
2024-09-01 12:57:54 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+def leap(year):
+    if ((year % 400==0)or(year % 100!=0 and year % 4==0)):
+        print("leap year")
+    else:
+        print("not leap year")
+year=int(input())
+leap(year)
+    
+    
+
+
+    
